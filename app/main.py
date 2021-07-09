bc4295db31284d2d217c17a2dbd61b4f395b1359
nichita.morcotilo <n.morcotilo@support.999.md>
2021-07-09 13:54:05 +0300

    Sort imports

--- app/main.py
+++ app/main.py
@@ -2,9 +2,8 @@ from fastapi import FastAPI
 from fastapi.exceptions import RequestValidationError
 from starlette.exceptions import HTTPException
 
-
-from app.api.errors.errors import http_error_handler
-from app.api.errors.errors import http_validation_error_handler
+from app.api.errors.errors import (http_error_handler,
+                                   http_validation_error_handler)
 from app.api.routes.api import router as api_router
 from app.core.config import API_PREFIX, DEBUG, PROJECT_NAME, VERSION
 from app.core.events import create_start_app_handler, create_stop_app_handler
@@ -24,8 +23,14 @@ def get_application() -> FastAPI:
 
     application.include_router(api_router, prefix=API_PREFIX)
 
-    application.add_exception_handler(HTTPException, http_error_handler)
-    application.add_exception_handler(RequestValidationError, http_validation_error_handler)
+    application.add_exception_handler(
+        HTTPException,
+        http_error_handler
+    )
+    application.add_exception_handler(
+        RequestValidationError,
+        http_validation_error_handler
+    )
 
     return application
 
